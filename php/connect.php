cde67cb6e8e1ed6f50223b15d0cd74d0c44b877c
camille <camilledds0@gmail.com>
2025-11-26 21:44:18 -0300

    fix: ajustando o host no connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,6 +1,5 @@
 <?php
-$pdo = new PDO('mysql:host=127.0.0.1:3307;dbname=SIMRD', 'root', '');
-// em casa o host=127.0.0.1:3307
+$pdo = new PDO('mysql:host=localhost;dbname=SIMRD', 'root', '');
 try{
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo -> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
